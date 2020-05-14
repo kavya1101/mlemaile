1a57824d8d5126dac2ff07b069e0d3e562152158
kavya1101 <55316419+kavya1101@users.noreply.github.com>
2020-05-14 12:44:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,39 +1,114 @@
-<html>
+
+<html lang="en">
 <head>
-<title>Hello PHP!</title>
-<script src="<?php echo $_ENV['ELASTICIO_WIDGET_URL'] ?>"></script>
+  <title>Simplitex.io</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="bootsrap/css/bootsrap.min.css">
+  <link rel="stylesheet" href="css/damn.css">
+
 </head>
 <body>
-<h1>PHP Demo of elastic.io contact importer</h1>
 
-<button onclick="elasticio.contacts()"><strong>Click me!</strong></button>
+    <nav class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle = "collapse" data-target ="#navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
 
-<p>This demo project demonstrate how you can connect your App to Xing,
- LinkedIn, Google Contacts, Salesforce and many other APIs to import
- (and synchronise) contact data.
- See more on <a href="http://www.elastic.io/contact_importer">here</a></p>
- 
-<h2>Imported contacts:</h2>
-<ul id="contacts"></ul>
-<script>
-	// Optional identification step
-	// see here for more information http://www.elastic.io/docs#identifying-widget-users
-	elasticio.identify({
-		id : 'my-superuser-id',
-		email : 'chuck@norris.com',
-		name : 'Chuck Norris'
-	});
-	// Registering event listener that will get the data
-	elasticio.on('contact', function(contact) {
-		// Now contact data is stored in 'contact' property
-		// exact format of the contact data see here - http://www.elastic.io/docs#canonical-contact-model
-		var para = document.createElement("li");
-		var node = document.createTextNode(contact.name.firstName + ' ' + contact.name.lastName);
-		para.appendChild(node);
+          </button>
+          <a class="navbar-brand" href="#">Simplitex.io</a>
+        </div>
+        
+     <div class="collapse navbar-collapse" id="navbar-collapse">
+       <ul class="nav navbar-nav navbar-right">
+         <li class="active"><a href="#">Home</a></li>
+         <li><a href="AboutUs.html">About Us</a></li>
+         <li><a href="LogIn.html">Login</a></li>
+         <li><a href="Register.html">Register</a></li>
 
-		var element = document.getElementById("contacts");
-		element.appendChild(para);
-	});
-</script>
+
+       </ul>
+     </div>   
+
+</div>
+</nav>
+
+  <div class="cover">
+    <div class="cover-text">
+      <h1>Learn Prepare & Interact with analysts,<br> managers, coders around the Globe!</h1> <br>
+      <a href="why-us" role="button" class="btn btn-danger btn-lg">Let's get started, I guess.</a>
+    </div>
+  </div>  
+
+  <section class="testimonial">
+    <div class="container">
+      <blockquote>
+        &ldquo; I never realised how bad I was before in analysing different scenerio. Simplitex.io crafted me into shapre, &amp; now i am confident in cracking any interviews which comes in front me. Highly recommended! &rdquo;
+        <cite>&mdash; A happy Client</cite>
+      </blockquote>
+    </div>
+  </section>
+
+  <section id="why-us">
+    <h2>Why Simplitex.io?</h2>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <h3>Amazing</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non bibendum quam. Aenean maximus lacus dapibus, sodales nulla vitae, molestie ante. Mauris nec nunc elementum, iaculis lacus ac, convallis mauris. Nam feugiat ligula justo. Morbi pellentesque orci leo, ac tristique tellus vestibulum nec. Nullam vulputate dolor erat, vel condimentum erat feugiat eget. Morbi tincidunt erat sit amet elit vehicula pulvinar.</p>
+        </div>
+         <div class="col-sm-4">
+          <h3>Marvelous</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non bibendum quam. Aenean maximus lacus dapibus, sodales nulla vitae, molestie ante. Mauris nec nunc elementum, iaculis lacus ac, convallis mauris. Nam feugiat ligula justo. Morbi pellentesque orci leo, ac tristique tellus vestibulum nec. Nullam vulputate dolor erat, vel condimentum erat feugiat eget. Morbi tincidunt erat sit amet elit vehicula pulvinar.</p>
+        </div>
+         <div class="col-sm-4">
+          <h3>Damn OP</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non bibendum quam. Aenean maximus lacus dapibus, sodales nulla vitae, molestie ante. Mauris nec nunc elementum, iaculis lacus ac, convallis mauris. Nam feugiat ligula justo. Morbi pellentesque orci leo, ac tristique tellus vestibulum nec. Nullam vulputate dolor erat, vel condimentum erat feugiat eget. Morbi tincidunt erat sit amet elit vehicula pulvinar. </p>
+        </div>
+      </div>
+    </div>
+  </section>  
+  
+
+  <section id="team">
+    <div class="container">
+      <h2> Meet our team</h2>
+      <div class="row">
+        <div class="col-sm-4">
+          <img src="img/Aditya.webp" alt="Women's face" class="img-circle">
+          <h4>XYZ XYZ</h4>
+          <p>He has completed his graduation from blah blah blahygd ffdff wfgf</p>
+        </div>
+        <div class="col-sm-4">
+          <img src="img/Aditya.webp" alt="Man's face" class="img-circle">
+          <h4>XYZ XYZ</h4>
+          <p>He has completed his graduation from blah blah blahygd ffdff wfgf</p>
+        </div>
+         <div class="col-sm-4">
+          <img src="img/Aditya.webp" alt="Women's face" class="img-circle">
+          <h4>XYZ XYZ</h4>
+          <p>He has completed his graduation from blah blah blahygd ffdff wfgf</p>
+        </div>
+        </div>
+
+    </div>
+    
+  </section>
+
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+
+
 </body>
 </html>
